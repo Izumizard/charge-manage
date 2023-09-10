dbf11101f4f219e06270bafc6f6908b0d9a9aa68
xiaoming <12983955+qwa3@user.noreply.gitee.com>
2023-09-10 21:21:41 +0800

    "version0.1"

--- manage/src/main.js
+++ manage/src/main.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import '@/assets/css/global.css'
+import '@/assets/css/global.css';
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 Vue.config.productionTip = false
-
+Vue.use(ElementUI,{size:'small'});
 new Vue({
   router,
   render: h => h(App)
